18ef0f8025dacf15474ec04ad8cdc7bcc37c3a2c
Ben <mbxian@xtremeprog.com>
2019-07-12 17:29:23 +0800

    init plugin

---
+++ src/main/java/com/xmb/ben/test/TestUtils.java
@@ -0,0 +1,14 @@
+package com.xmb.ben.test;
+
+/**
+ * Author by Ben
+ * On 2019-07-12.
+ *
+ * @Descption
+ */
+public class TestUtils {
+
+    public static String getTestString() {
+        return "testString";
+    }
+}
